e98e8bee17aaabe0b07bd79f0b842b36fffbf5a9
emersonope <emersonope@gmail.com>
2022-09-17 20:42:43 -0300

    adjusting card 2nd screen

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Image, TouchableOpacity, View, FlatList } from 'react-native';
+import { Image, TouchableOpacity, View, FlatList, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -75,9 +75,16 @@ export function Game() {
             />
           )}
           horizontal
-          contentContainerStyle={styles.contentList}
-          showsHorizontalScrollIndicator={false}
           style={styles.containerList}
+          contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados para esse jogo!
+            </Text>
+
+          )}
+          
         />
 
       </SafeAreaView>
